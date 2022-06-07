01cb0765cc929dc6515ae2a187a4e834f7718792
Leonardo Sales <leonardosales.silva12@gmail.com>
2022-06-07 17:36:51 -0300

    adjust general

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 const elementNav = document.getElementById('navigation')
 
-const button = document.getElementById('btn-open')
+const buttonOpenMenu = document.getElementById('btn-open')
 
-button.addEventListener('click', event => {
+buttonOpenMenu.addEventListener('click', event => {
   if (elementNav.classList.contains('active')) {
     elementNav.classList.remove('active')
   } else {
     elementNav.classList.add('active')
   }
 })
+
+const boxTicketsNumber = document.querySelectorAll('.number-box')
+
+for (var i = 0; i < boxTicketsNumber.length; i++) {
+  boxTicketsNumber[i].onmouseover = function () {
+    this.querySelector('span').style.display = 'inline-block'
+  }
+  boxTicketsNumber[i].onmouseout = function () {
+    this.querySelector('span').style.display = 'none'
+  }
+}
